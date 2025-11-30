c8960dded6907f498cf1ac40e6070f56e4f9443e
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2025-12-01 00:02:17 +0530

    enhancing blog and adding social features

--- Blog_site/blog/models.py
+++ Blog_site/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -10,7 +11,7 @@ class Post(models.Model):
         DRAFT='DF','DRAFT'
         PUBLISHED='PB','PUBLISHED'
     title=models.CharField(max_length=250)
-    slug=models.SlugField(max_length=250)
+    slug=models.SlugField(max_length=250, unique_for_date='publish')
     author=models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -33,6 +34,8 @@ class Post(models.Model):
         indexes=[
             models.Index(fields=['-publish'])
         ]
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
 
     def __str__(self):
         return self.title
--- Blog_site/blog/views.py
+++ Blog_site/blog/views.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
+from .views import EmailPostForm
 # Create your views here.
-def post_list(request):
-    posts=Post.published.all()
-    return render(request,'blog/post/list.html',{'posts':posts})
+# def post_list(request):
+#     post_list=Post.published.all()
+#     #pagination with 3 post per page
+#     paginator=Paginator(post_list,3)
+#     page_number=request.GET.get('page',1)
+#     try:
+#         posts=paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts=paginator.page(1)
+#     except EmptyPage:
+#         posts=paginator.page(paginator.num_pages)
+#     return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request,id):
+def post_detail(request,year,month,day,post):
     try:
-        post=Post.published.get(id=id)
+        post=Post.published.get(slug=post,publish__year=year,publish__month=month,publish__day=day)
     except Post.DoesNotExist:
         raise Http404("No Post found")
     return render(request,'blog/post/detail.html',{'post':post})
+
+
+class PostListView(ListView):
+    queryset=Post.published.all()
+    context_object_name='posts'
+    paginate_by=3
+    template_name='blog/post/list.html'
+
+def post_share(request,post_id):
+    #retrieve post by id
+    post=get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    if request.method=='POST':
+        #Form was submitted
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            #form fields pass validation
+            cd=form.cleaned_data
+        
+    else:
+        form=EmailPostForm()
+    return render(request,'blog/post/share.html',{'post':post,'form':form})
